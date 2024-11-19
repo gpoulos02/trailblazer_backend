f8578426128d51d4efdff64e8ecffd07a0cf883d
Karim Taha <ktaha2@uwo.ca>
2024-11-19 13:01:31 -0500

    Change to bmt.js schema

--- models/BlueMountainTrail.js
+++ models/BlueMountainTrail.js
@@ -5,29 +5,27 @@ const Schema = mongoose.Schema;
 const trailSchema = new Schema({
   runID: { type: Number, required: true, unique: true },
   runName: { type: String, required: true },
-  difficulty: { type: String, enum: ['Green', 'Blue', 'Black', 'Double Black'], required: true },
-  runLength: { type: Number, required: true },  // Length in meters
-  destinations: [{ type: Schema.Types.ObjectId, ref: 'PointOfInterest' }],  // Refers to POIs
-  coordinates: {
-    long: { type: Number, required: true }, // Longitude
-    lat: { type: Number, required: true }   // Latitude
-  }
-});
-
-// Create a model from the schema
-const BlueMountainTrail = mongoose.model('BlueMountainTrail', trailSchema);
-
-module.exports = BlueMountainTrail;
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-// Trail schema
-const trailSchema = new Schema({
-  runID: { type: Number, required: true, unique: true },
-  runName: { type: String, required: true },
-  difficulty: { type: String, enum: ['Green', 'Blue', 'Black', 'Double Black'], required: true },
-  runLength: { type: Number, required: true },  // Length in meters
-  destinations: [{ type: Schema.Types.ObjectId, ref: 'PointOfInterest' }],  // Refers to POIs
+  difficulty: { 
+    type: String, 
+    enum: ['Green', 'Blue', 'Black', 'Double Black'], 
+    required: true 
+  },
+  startingPoints: [
+    {
+      chairlifts: [{ type: Schema.Types.ObjectId, ref: 'PointOfInterest' }], // Chairlifts going into it
+      runs: [{ type: Number }] // runIDs connecting to this starting point
+    }
+  ],
+  endingPoints: [
+    {
+      type: {
+        type: String,
+        enum: ['Lodging', 'Parking Lot', 'Other', 'Chairlift', 'Restaurant', 'Run'],
+        required: true
+      },
+      POI_id: { type: Number, required: true } // Reference to the point of interest's ID
+    }
+  ],
   coordinates: {
     long: { type: Number, required: true }, // Longitude
     lat: { type: Number, required: true }   // Latitude
