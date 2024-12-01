3f6610b0997bfddf846b6f1e300592151869ce5f
emmakturk <114186999+emmakturk@users.noreply.github.com>
2024-12-01 16:10:41 -0500

    Change server name

--- routes/metricsRoutes.js
+++ routes/metricsRoutes.js
@@ -9,13 +9,13 @@ router.post('/', authMiddleware, metricsController.saveSession);
 // GET /api/metrics/dates - Get a list of session dates
 router.get('/dates', authMiddleware, metricsController.getSessionDates);
 
+// GET /api/metrics/overview - Get an overview of metrics over a specified period
+router.get('/overview', authMiddleware, metricsController.getMetricOverview);
+
 // GET /api/metrics/:id - Get session data for a specific session
 router.get('/:id', authMiddleware, metricsController.getSessionById);
 
 // DELETE /api/metrics/:id - Delete a specific session
 router.delete('/:id', authMiddleware, metricsController.deleteSession);
-
-// GET /api/metrics/overview - Get an overview of metrics over a specified period
-router.get('/overview', authMiddleware, metricsController.getMetricOverview);
 
 module.exports = router;
