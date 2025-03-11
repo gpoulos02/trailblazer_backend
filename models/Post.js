2ac1da4971d22fa15218fd732f7a317d941779cd
emmakturk <114186999+emmakturk@users.noreply.github.com>
2025-03-11 18:43:13 -0400

    fix

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 
 const postSchema = new mongoose.Schema({
     postID: { type: String, default: uuidv4, unique: true, required: true }, 
-    userID: { type: String, ref: 'User', required: true }, // Ensure this is a String (UUID)
+    userID: { type: String, ref: 'User', required: true }, // Store userID as a String
+
     type: { 
         type: String, 
         enum: ['text', 'route', 'performance'], 
@@ -15,7 +16,7 @@ const postSchema = new mongoose.Schema({
     likes: [{ type: String}], // Store likes using userID (String)
 
     comments: [{
-        username: { type: String, required: true }, 
+        user: { type: String, ref: 'User', required: true },
         content: { type: String, required: true },
         createdAt: { type: Date, default: Date.now }
     }],
